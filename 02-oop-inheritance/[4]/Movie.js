bd2839f0e575dd9d8af90248921dbaab6732ed51
matoliva <olivamati@gmail.com>
2018-07-03 17:18:01 -0300

    I updated the movie class with the addCast method

--- 02-oop-inheritance/[4]/Movie.js
+++ 02-oop-inheritance/[4]/Movie.js
@@ -7,6 +7,7 @@ export default class Movie extends EventEmmiter{
         this.title = title;
         this. year = year;
         this.duration = duration;
+        this.cast = [];
     }
 
     play(){
@@ -20,12 +21,22 @@ export default class Movie extends EventEmmiter{
     resume(){
         super.emit('resume');
     }
+
+    addCast(actor){
+        if(Array.isArray(actor))
+        {
+            actor.forEach(auxCast => this.cast.push(auxCast));
+        }
+        else{
+            this.cast.push(actor);
+        }
+    }
 }
 
-let harryPotter = new Movie("shrek", 2010, 120);
+/*let harryPotter = new Movie("shrek", 2010, 120);
 
 harryPotter.on('play', () => {console.log("playing " + this.title)});
 harryPotter.on('pause', () => {console.log("pause " + this.title)});
 harryPotter.on('resume', () => {console.log("resume " + this.title)});
 
-harryPotter.play();
+harryPotter.play();*/
---
+++ 02-oop-inheritance/[7]/Actor.js
@@ -0,0 +1,11 @@
+
+class Actor{
+    constructor(fullName, age){
+        this.fullName = fullName;
+        this.age = age;
+    }
+}
+
+/*const arnold = new Actor('Arnold Schwarzenegger', 70);
+
+console.log(arnold.fullName);*/
